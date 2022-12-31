019c7918225f99643d6fea47b7c0b1118444bc6b
Andrei Savu <savu.andrei96@outlook.com>
2022-12-31 17:32:25 +0200

    Take out env

--- Bogvelion2022/src/environments/environment.ts
+++ Bogvelion2022/src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyCYUEWmiFuAnsU5x3f67fBFqfN_JscH-Bk",
-    authDomain: "bogvelion2022.firebaseapp.com",
-    projectId: "bogvelion2022",
-    storageBucket: "bogvelion2022.appspot.com",
-    messagingSenderId: "489178298100",
-    appId: "1:489178298100:web:4097325044b6dafffd10a1"
-  }
+  production: false
 };
 
 /*
